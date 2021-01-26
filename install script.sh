c2018d7f36010878fbebd7a325c067fb083f4332
dsimonet <donald@dsimonet.fr>
2021-01-26 11:15:52 +0100

    added if for sublime & add sshfs

--- install script.sh
+++ install script.sh
@@ -1,17 +1,19 @@
 cd ~
 
-
-
 sudo apt-get update 
-sudo apt install -y tmux putty
+sudo apt install -y tmux putty sshfs
 sudo apt install -y openocd gcc-arm-none-eabi binutils-arm-none-eabi libnewlib-arm-none-eabi
 
 #sublime text
-#wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-#sudo apt-get install apt-transport-https
-#echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-#sudo apt-get update
-#sudo apt-get install sublime-text -y
+if [ -f "/etc/apt/sources.list.d/sublime-text.list" ]; then
+    echo "Sublime texte already installed, skip"
+else
+	wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+	sudo apt-get install apt-transport-https
+	echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+	sudo apt-get update
+	sudo apt-get install sublime-text -y	
+fi
 
 #git
 sudo apt install -y git
